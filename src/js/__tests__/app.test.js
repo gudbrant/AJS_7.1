538feaa63ebd9f4320714b1b1653189678acc07c
gudbrant <61345734+gudbrant@users.noreply.github.com>
2020-12-29 11:18:39 +0800

    0.0.3

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,8 +1,50 @@
 import Validator from '../app';
 
-test('Проверка имени пользователя MaxPayne', () => {
-  const newUser = new Validator('Max72Payne');
-  const received = newUser.validateUsername();
-  const expected = true;
-  expect(received).toEqual(expected);
+test('Проверка правильного формата имени', () => {
+  const result = new Validator('An-12_07-ton').validateUsername();
+
+  expect(result).toBe('An-12_07-ton');
 });
+
+test('Проверка неправильного формата имени', () => {
+  const result = new Validator('12-An_07-ton').validateUsername();
+
+  expect(result).toBe('Неверный формат имени');
+});
+
+test('Проверка неправильного формата имени', () => {
+  const result = new Validator('An-12_ton-07').validateUsername();
+
+  expect(result).toBe('Неверный формат имени');
+});
+
+test('Проверка неправильного формата имени', () => {
+  const result = new Validator('An_1207-ton').validateUsername();
+
+  expect(result).toBe('Неверный формат имени');
+});
+
+test('Проверка неправильного формата имени', () => {
+  const result = new Validator('-An12_07-ton').validateUsername();
+
+  expect(result).toBe('Неверный формат имени');
+});
+
+test('Проверка неправильного формата имени', () => {
+  const result = new Validator('An-12_07-ton_').validateUsername();
+
+  expect(result).toBe('Неверный формат имени');
+});
+
+test('Проверка неправильного формата имени', () => {
+  const result = new Validator('Ан-12_07-тон').validateUsername();
+
+  expect(result).toBe('Неверный формат имени');
+});
+
+// test('Проверка имени пользователя MaxPayne', () => {
+//   const newUser = new Validator('Max72Payne');
+//   const received = newUser.validateUsername();
+//   const expected = true;
+//   expect(received).toEqual(expected);
+// });
--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,19 @@ export default class Validator {
   }
 
   validateUsername() {
-    const val1 = /^[\w-]+$/.test(this.username);
-    const val2 = /\d{3}/.test(this.username);
-    const val3 = /^[^\d-\_]/.test(this.username);
-    const val4 = /[^\d-\_]$/.test(this.username);
-
-    return ((val1 === val3 === val4 === true) && (val2 === false));
+    if (/\d{4,}/.test(this.username) || !/^[a-z][\w-]*[a-z]$/i.test(this.username)) {
+      return 'Неверный формат имени';
+    } else {
+      return this.username;
+    }
   }
+
+  // validateUsername() {
+  //   const val1 = /^[\w-]+$/.test(this.username);
+  //   const val2 = /\d{3}/.test(this.username);
+  //   const val3 = /^[^\d-\_]/.test(this.username);
+  //   const val4 = /[^\d-\_]$/.test(this.username);
+  //
+  //   return ((val1 === val3 === val4 === true) && (val2 === false));
+  // }
 }
